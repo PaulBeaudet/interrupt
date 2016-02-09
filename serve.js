3db766a40d48a0d50812af9ceada75e745f60343
paulBeaudet <inof8or@gmail.com>
2016-02-09 17:34:45 -0500

    Have participants use names instead of socket ids

--- serve.js
+++ serve.js
@@ -5,14 +5,16 @@ var when = {
     idle: true,
     users: 0,
     connected: function(socket){
-        console.log(socket + ' connected');
-        sock.ets.to(socket).emit('youAre', socket); // make sure the socket knows who it is
+        var cookieCrums = socket.request.headers.cookie.split('=');  // split correct cookie out
+        var user = cookie.user(cookieCrums[cookieCrums.length - 1]);  // decrypt email from cookie, make it userID
+        console.log(user.name + ' connected id:' + socket.id);
+        sock.ets.to(socket.id).emit('youAre', user.name); // make sure the socket knows who it is
         when.users++;
         if(when.users > 1 && when.idle){
             sock.ets.emit('go', {text: 'start typing!', id: 'server'});
             when.idle = false;
         }
-        return 0;
+        return user.name;
     },
     chat: function(rtt){sock.ets.emit('chat', rtt);},
     disconnect: function(socket){
@@ -27,13 +29,32 @@ var sock = {
     listen: function(server){
         sock.ets = sock.ets(server);
         sock.ets.on('connection', function(socket){
-            var nfo = when.connected(socket.id);
-            socket.on('chat', function(text){sock.ets.emit('chat', {text: text, id: socket.id});});
+            var name = when.connected(socket);
+            socket.on('chat', function(text){sock.ets.emit('chat', {text: text, id: name});});
             socket.on('go', function(){sock.ets.emit('go');});
             //
             socket.on('disconnect', function(){when.disconnect(socket.id);});
         });
     },
+}
+
+var userAct = { // dep: mongo
+    auth: function ( render ){
+        return function(req, res){
+            if(req.session.user.name){
+                console.log(req.session.user.name + ' joined')
+                res.render(render);
+            } else {res.redirect('/');}
+        }
+    },
+    name: function ( req, res ){
+        if(req.body.name){
+            req.session.user = {name: req.body.name};
+            res.redirect('/chat');
+        } else {
+            res.redirect('/');
+        }
+    }
 }
 
 var cookie = { // depends on client-sessions and mongo
@@ -44,7 +65,6 @@ var cookie = { // depends on client-sessions and mongo
         duration: 8 * 60 * 60 * 1000,  // cookie times out in 8 hours
         activeDuration: 5 * 60 * 1000, // activity extends durration 5 minutes
         httpOnly: true,                // block browser access to cookies... defaults to this anyhow
-        //secure: true,                // only allow cookies over HTTPS
     },
     meWant: function (){return cookie.session(cookie.ingredients);},
     user: function (content){
@@ -67,7 +87,9 @@ var serve = {
 
         app.use(serve.express.static(__dirname + '/views')); // serve page dependancies (sockets, jquery, bootstrap)
         var router = serve.express.Router();
-        router.get('/', function(req, res){res.render('chat');});
+        router.get('/', function(req, res){res.render('name');});
+        router.post('/', userAct.name);
+        router.get('/chat', userAct.auth('chat'));
         app.use(router);
 
         sock.listen(http);                            // listen for socket connections
